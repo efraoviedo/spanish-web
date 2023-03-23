f0f7f6467c85fe140a5aa34976bf25e61024cca7
efranor <efraoviedo@gmail.com>
2023-03-23 18:46:23 -0500

    change name

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Norefront Webs
+              Ovioto Webs
             </span><br />
             <span className="cursor-pointer font-mono whitespace-nowrap
             border-right-4 w-12 animate-pulse overflow-visible text-white">Diseñamos Aplicaciones Paginas Web
