52ee72935c00624c3a0764035b33f9e44adf988b
efranor <efraoviedo@gmail.com>
2023-03-26 17:50:50 -0500

    erase home module

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
+
+module.exports = {
   reactStrictMode: true,
 };
-
-module.exports = nextConfig;
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,15 @@
+// module.exports = {
+//   plugins: {
+//     "postcss-import": {},
+//     "tailwindcss/nesting": "postcss-nesting",
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
 module.exports = {
   plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
