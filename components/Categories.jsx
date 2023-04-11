d206f44036bec2a6f7698881ad781ef28d9f8742
efranor <efraoviedo@gmail.com>
2023-04-11 04:03:38 -0500

    I changed to node 18.13.0

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -14,11 +14,11 @@ const Categories = () => {
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categorias</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4 hover:bg-slate-100">Categorias</h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
           <span
-            className={`cursor-pointer block ${
+            className={`cursor-pointer hover:bg-slate-200 hover:text-cyan-700 block ${
               index === categories.length - 1 ? "border-b-0" : "border-b"
             } pb-3 mb-3`}
           >
